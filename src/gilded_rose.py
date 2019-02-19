f5d70cedf566721a97e852421e6b77aaaf72181d
Dylan Davis <djdavis1420@gmail.com>
2019-02-19 17:10:49 -0600

    testing revert

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -31,9 +31,7 @@ class GildedRose(object):
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+
 
 
 class Item:
