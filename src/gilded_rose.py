5aa46bb564a2f532dbef57a3f5db7876a338a095
Dylan Davis <djdavis1420@gmail.com>
2019-02-20 18:09:33 -0600

    Divided Conditionals & Strengthened Update_Item Function

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -1,43 +1,49 @@
 # -*- coding: utf-8 -*-
 
 
-
 class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.name != "Sulfuras, Hand of Ragnaros":
-                    self.__update_item(item, 'dec')
-                    if "Conjured" in item.name:
-                        self.__update_item(item, 'dec')
-            else:
+
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+
+            elif item.name == "Aged Brie":
                 self.__update_item(item, 'inc')
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        self.__update_item(item, 'inc')
-                    if item.sell_in < 6:
-                        self.__update_item(item, 'inc')
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.name != "Sulfuras, Hand of Ragnaros":
-                            self.__update_item(item, 'dec')
-                            if "Conjured" in item.name:
-                                self.__update_item(item, 'dec')
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+                if item.sell_in <= 0:
                     self.__update_item(item, 'inc')
 
-    def __update_item(self, item, direction):
-        if item.quality < 50 and direction == 'inc':
-            item.quality += 1
-        if item.quality > 0 and direction == 'dec':
-            item.quality -= 1
-        return item.quality
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.__update_item(item, 'inc')
+                if item.sell_in < 11:
+                    self.__update_item(item, 'inc')
+                if item.sell_in < 6:
+                    self.__update_item(item, 'inc')
+                if item.sell_in <= 0:
+                    item.quality = 0
 
+            elif "Conjured" in item.name:
+                if item.sell_in > 0:
+                    self.__update_item(item, 'dec', 2)
+                elif item.sell_in <= 0:
+                    self.__update_item(item, 'dec', 4)
+
+            else:
+                if item.sell_in <= 0:
+                    self.__update_item(item, 'dec', 2)
+                elif item.quality > 0:
+                    self.__update_item(item, 'dec')
+
+            item.sell_in -= 1
+
+    def __update_item(self, item, direction, amount=1):
+        while item.quality < 50 and direction == 'inc' and amount >= 1:
+            item.quality += 1
+            amount -= 1
+        while item.quality > 0 and direction == 'dec' and amount >= 1:
+            item.quality -= 1
+            amount -= 1
+        return item.quality
